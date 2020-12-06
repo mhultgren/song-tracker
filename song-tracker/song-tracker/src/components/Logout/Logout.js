1912771c004500a24dcc29cbb567044e28707258
Myles Hultgren <myleshultgren@gmail.com>
2020-12-05 19:03:38 -0800

    Update Logout.js

--- song-tracker/song-tracker/src/components/Logout/Logout.js
+++ song-tracker/song-tracker/src/components/Logout/Logout.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import './Logout.css';
 import { GoogleLogout } from 'react-google-login';
 
-const clientId = '61064734999-7a06si2l6d02kpvd21o6sioc0a3aqqt2.apps.googleusercontent.com';
-
 class Logout extends Component {
     render() {
         const onSuccess = ( ) => {
